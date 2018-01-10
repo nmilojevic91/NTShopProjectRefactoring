17a7ac86f2924a910f4b7b31d6a6f5853ce58071
nemmilojevic91 <n.milojevic@ymail.com>
2018-01-10 19:07:22 +0100

    Added Nunit project and few tests

---
+++ TheShop/TheShopUnitTests/OrderAndSellArticleTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using TheShop;
+
+namespace TheShopUnitTests
+{
+	public class OrderAndSellArticleTests
+	{
+		[TestCase(1, 20, 10)]
+		[TestCase(1, 100, 10)]
+		[TestCase(1, -50, 12)]
+		[TestCase(1, 20, 13)]
+		[TestCase(1, 20, 14)]
+		public void TestOrderAndSellArticleSuccess(int id, int maxPrice, int buyerId)
+		{
+			//prepare 
+
+			ShopService service = new ShopService();
+			Exception thrownException = null;
+			//act
+			try
+			{
+				service.OrderAndSellArticle(id, maxPrice, buyerId);
+			}
+			catch(Exception e)
+			{
+				thrownException = e;
+			}
+			//assert
+			Assert.IsNull(thrownException);
+		}
+
+		[TestCase(2, 100, 10)]
+		[TestCase(3, -50, 12)]
+		[TestCase(4, 20, 13)]
+		[TestCase(-1, 20, 14)]
+		public void TestOrderAndSellArticleNonExistingArticle(int id, int maxPrice, int buyerId)
+		{
+			//prepare 
+			ShopService service = new ShopService();
+			Exception thrownException = null;
+
+			//act
+			try
+			{
+				service.OrderAndSellArticle(id, maxPrice, buyerId);
+			}
+			catch (Exception e)
+			{
+				thrownException = e;
+			}
+
+			//assert
+			Assert.IsNull(thrownException);
+		}
+	}
+}
---
+++ TheShop/TheShopUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TheShopUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheShopUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c3bcce0-2f96-41be-bbab-847c986ffe4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
